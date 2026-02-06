eeda9f2b7241722cfd77268adeaa3fce36e113fb
AtlcatlQ <quintanillajoseh2003@gmail.com>
2026-02-06 16:38:52 -0600

    ♻️ Workflow (🃏 Templates): ADD default sections in Note Types in Templater Scripts

--- A-Utility/A.A-Scripts/templates/noteTypes/noteTypes.js
+++ A-Utility/A.A-Scripts/templates/noteTypes/noteTypes.js
@@ -9,20 +9,23 @@ module.exports = () => {
 		// ㊙️ PERSONAL
 		"personal": {
 			"label": "㊙️ Personal",
-			"folder": "5-REGISTRO", 
-			"handler": async (tp, template) => await tp.user.personalHandler(tp, template), 
+			"folder": "5-REGISTRO",
+			"handler": async (tp, template) => await tp.user.personalHandler(tp, template),
+			"sections": [],
 		},
 
 		// 📔 P. A. R. A.
 		"area": {
 			"label": "🪐 ÁREA",
-			"folder": "2-AREAS", 
-			"handler": async (tp, template) => await tp.user.areaHandler(tp, template), 
+			"folder": "2-AREAS",
+			"handler": async (tp, template) => await tp.user.areaHandler(tp, template),
+			"sections": ["tasks", "projects", "areas", "notas", "fuentes"],
 		},
 		"project": {
 			"label": "🏹 PROYECTO",
-			"folder": "1-PROJECTS", 
-			"handler": async (tp, template) => await tp.user.projectHandler(tp, template), 
+			"folder": "1-PROJECTS",
+			"handler": async (tp, template) => await tp.user.projectHandler(tp, template),
+			"sections": ["tasks", "projects", "notas", "fuentes"],
 		},
 		*/
 
@@ -31,49 +34,56 @@ module.exports = () => {
 			"label": "📃 Documentación Interna",
 			"folder": "6-Flujo/6.0-Documentacion-Interna",
 			"handler": async (tp, template) => await tp.user.documentationHandler(tp, template),
+			"sections": [],
 		},
 
 		/*
 		// ⚙️ CONFIGURATION
 		"classes": {
 			"label": "🗂️ Clase",
-			"folder": "A-Utility/A.1-Classes", 
-			"handler": async (tp, template) => await tp.user.classesHandler(tp, template), 
+			"folder": "A-Utility/A.1-Classes",
+			"handler": async (tp, template) => await tp.user.classesHandler(tp, template),
+			"sections": [],
 		},
 		"plugin": {
 			"label": "🔌 Revisión de Plugin",
-			"folder": "A-Utility/A.3-Plugins", 
-			"handler": async (tp, template) => await tp.user.pluginHandler(tp, template), 
+			"folder": "A-Utility/A.3-Plugins",
+			"handler": async (tp, template) => await tp.user.pluginHandler(tp, template),
+			"sections": ["indexCard", "documentation", "settings", "style"],
 		},
 		"theme": {
 			"label": "💄 Revisión de Tema",
-			"folder": "A-Utility/A.4-Temas", 
-			"handler": async (tp, template) => await tp.user.themeHandler(tp, template), 
+			"folder": "A-Utility/A.4-Temas",
+			"handler": async (tp, template) => await tp.user.themeHandler(tp, template),
+			"sections": ["indexCard", "documentation", "settings", "style"],
 		},
 		"snippets": {
 			"label": "💄 Revisión de CSS Snippets",
-			"folder": "A-Utility/A.5-Snippets", 
-			"handler": async (tp, template) => await tp.user.snippetsHandler(tp, template), 
+			"folder": "A-Utility/A.5-Snippets",
+			"handler": async (tp, template) => await tp.user.snippetsHandler(tp, template),
+			"sections": ["indexCard"],
 		},
 
 		// ✍️ WRITING
 		"writing": {
 			"label": "✍️ Escrito",
-			"folder": "7-Crisol-de-Escritura/7.A-Manuscritos", 
-			"handler": async (tp, template) => await tp.user.writingHandler(tp, template), 
+			"folder": "7-Crisol-de-Escritura/7.A-Manuscritos",
+			"handler": async (tp, template) => await tp.user.writingHandler(tp, template),
+			"sections": [],
 		},
 
 		// 🗃️ ZETTELKASTEN
 		"capture": {
 			"label": "📥 Nota de Captura",
-			"folder": "0-INBOX/0.1-Captura", 
-			"handler": async (tp, template) => await tp.user.captureHandler(tp, template), 
-			
+			"folder": "0-INBOX/0.1-Captura",
+			"handler": async (tp, template) => await tp.user.captureHandler(tp, template),
+			"sections": ["indexCard", "notas"],
 		},
 		"permanent": {
 			"label": "📓 Nota Permanente",
-			"folder": "0-INBOX", 
-			"handler": async (tp, template) => await tp.user.noteHandler(tp, template), 
+			"folder": "0-INBOX",
+			"handler": async (tp, template) => await tp.user.noteHandler(tp, template),
+			"sections": [],
 		},
 		"default": {
 			// Este tipo es especial porque en realidad es un subtipo de nota de captura
@@ -81,8 +91,9 @@ module.exports = () => {
 			// Ya que una nota rápida en Zettelkasten no es una nota permanente
 			// Y aunque no tenga referencia, me ayuda a sacar mis pensamientos rápido
 			"label": "✏️ Apunte Rápido",
-			"folder": "", 
-			"handler": async (tp, template) => await tp.user.captureHandler(tp, template), 
+			"folder": "",
+			"handler": async (tp, template) => await tp.user.captureHandler(tp, template),
+			"sections": [],
 		},
 		*/
 	};
