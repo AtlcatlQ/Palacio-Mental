100e7a40e6a5632092b7845971498cd49adc2e08
AtlcatlQ <quintanillajoseh2003@gmail.com>
2026-02-06 15:26:54 -0600

    ♻️ Workflow (🃏 Templates): ADD commented sections for templater scripts
    The sections aren't implemented yet, they are just declared.

--- A-Utility/A.A-Scripts/templates/noteSections/noteSections.js
+++ A-Utility/A.A-Scripts/templates/noteSections/noteSections.js
@@ -21,15 +21,54 @@ module.exports = () => {
 		"indexCard": {
 			"label": "📇 Ficha",
 			"content": async (tp, template) => await tp.user.indexcardRender(tp, template)
-			},	
-		"documentation": {
-			"label": "📃 Documentación",
-			"content": async (tp, template) => await tp.user.documentationRender(tp, template)
+		},
+		/*
+		"tasks": {
+			"label": "📌 Agenda",
+			"content": async (tp, template) => await tp.user.tasksRender(tp, template)
+		},
+		"areas": {
+			"label": "🪐 Áreas",
+			"content": async (tp, template) => await tp.user.areasRender(tp, template)
+		},
+		"projects": {
+			"label": "🏹 Proyectos",
+			"content": async (tp, template) => await tp.user.projectsRender(tp, template)
+		},
+		"notes": {
+			"label": "📓 Notas",
+			"content": async (tp, template) => await tp.user.notesRender(tp, template)
+		},
+		
+		"jotting": {
+			"label": "✏️ Apuntes", 
+			"content": async (tp, template) => await tp.user.jottingRender(tp, template)
+		},
+		"idea": {
+			"label": "💡 Idea",
+			"content": async (tp, template) => await tp.user.ideaRender(tp, template)
+		},
+		"question": {
+			"label": "❓ Pregunta",
+			"content": async (tp, template) => await tp.user.questionRender(tp, template)
+		},
+		"extract": {
+			"label": "📖 Extracto",
+			"content": async (tp, template) => await tp.user.extractRender(tp, template)
+		},
+		"reflection": {
+			"label": "💭 Pensamiento",
+			"content": async (tp, template) => await tp.user.reflectionRender(tp, template)
+		},
+		*/
+		"thought": {
+			"label": "label",
+			"content": async (tp, template) => await tp.user.thoughtRender(tp, template)
 		},
 		"settings": {
 			"label": "⚙️ Configuración",
 			"content": async (tp, template) => await tp.user.settingsRender(tp, template)
-			},
+		},
 		"documentation": {
 			"label": "📃 Documentación",
 			"content": async (tp, template) => await tp.user.documentationRender(tp, template)
@@ -38,12 +77,6 @@ module.exports = () => {
 			"label": "💄 Style",
 			"content": async (tp, template) => await tp.user.styleRender(tp, template)
 		},
-		/*
-		"apuntes": {
-			"label": "✏️ Apuntes", 
-			"content": async (tp, template) => await tp.user.apuntesRender(tp, template)
-		},
-		*/
 	};
 
 	return Object.freeze(sections);
