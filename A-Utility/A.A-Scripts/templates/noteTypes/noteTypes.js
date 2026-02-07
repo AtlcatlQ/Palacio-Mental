6064f0a9808c08403005b540778d511485a5cd2a
AtlcatlQ <quintanillajoseh2003@gmail.com>
2026-02-07 16:14:44 -0600

    ♻️ Workflow (🃏 Templates): ADD field definitions for Note Types in Templater Scripts
    Default fields, field default values and specific field options per type

--- A-Utility/A.A-Scripts/templates/noteTypes/noteTypes.js
+++ A-Utility/A.A-Scripts/templates/noteTypes/noteTypes.js
@@ -1,89 +1,187 @@
 /**
  * @description Diccionario de Tipos de Notas
- * @returns {Object} Definición de los distintos Tipos de Notas
+ * @returns {Object} Definición de Tipos de Ficheros (Tipos Base)
  */
 
 module.exports = () => {
 	const types = {
 		/*
 		// ㊙️ PERSONAL
-		"personal": {
+		"Personal": {
 			"label": "㊙️ Personal",
 			"folder": "5-REGISTRO",
 			"handler": async (tp, template) => await tp.user.personalHandler(tp, template),
 			"sections": [],
+			"fields": [],
+			"defaults": {
+				"tags": "㊙️",
+				"publish": false,
+			}
 		},
 
 		// 📔 P. A. R. A.
-		"area": {
+		"Area": {
 			"label": "🪐 ÁREA",
 			"folder": "2-AREAS",
 			"handler": async (tp, template) => await tp.user.areaHandler(tp, template),
 			"sections": ["tasks", "projects", "areas", "notas", "fuentes"],
+			"fields": [],
+			"defaults": {
+				"tags": "🪐",
+				"publish": false
+			},
 		},
-		"project": {
+		"Project": {
 			"label": "🏹 PROYECTO",
 			"folder": "1-PROJECTS",
 			"handler": async (tp, template) => await tp.user.projectHandler(tp, template),
 			"sections": ["tasks", "projects", "notas", "fuentes"],
+			"fields": [],
+			"defaults": {
+				"tags": "🏹",
+				"publish": false,
+			},
 		},
 		*/
 
 		// ♻️ WORKFLOW
-		"documentation": {
+		"Documentation": {
 			"label": "📃 Documentación Interna",
 			"folder": "6-Flujo/6.0-Documentacion-Interna",
 			"handler": async (tp, template) => await tp.user.documentationHandler(tp, template),
 			"sections": [],
+			"fields": [],
+			"defaults": {
+				"tags": "📃",
+			},
 		},
 
 		/*
 		// ⚙️ CONFIGURATION
-		"classes": {
+		"Classes": {
 			"label": "🗂️ Clase",
 			"folder": "A-Utility/A.1-Classes",
 			"handler": async (tp, template) => await tp.user.classesHandler(tp, template),
 			"sections": [],
+			"fields": [],
+			"default": {
+				"extends": "generic",
+				"tags": "⚙️/🗂️",
+				"mapWithTag": true,
+			},
 		},
-		"plugin": {
+		"Plugin": {
 			"label": "🔌 Revisión de Plugin",
 			"folder": "A-Utility/A.3-Plugins",
 			"handler": async (tp, template) => await tp.user.pluginHandler(tp, template),
 			"sections": ["indexCard", "documentation", "settings", "style"],
+			"fields": ["url", "version", "uses", "platform"],
+			"defaults": {
+				"tags": "⚙️/🔌",
+				"status": "🔲",
+			},
+			"status": {
+				"Pendiente": "🔲",
+				"En Revisión": "🔳",
+				"Activo": "✔️",
+				"Inactivo": "❌", 
+			},
+			"uses": {
+				"Layout": "💻",
+				"Hotkeys": "⌨️",
+				"Plugins": "🔌",
+				"Gestión de Archivos": "📂",
+				"Gestión de Imágenes": "🖼️",
+				"Integration": "🧩",
+				"Formatting": "🔦",
+				"UI": "💄",
+				"Metadata": "🔑",
+				"Templates": "🃏",
+				"Workflow": "♻️",
+				"Tareas": "📌",
+				"Diary": "📒",
+				"Habits": "🔂",
+				"Information": "📥",
+				"Automation": "🔄️",
+			},
 		},
-		"theme": {
+		"Theme": {
 			"label": "💄 Revisión de Tema",
 			"folder": "A-Utility/A.4-Temas",
 			"handler": async (tp, template) => await tp.user.themeHandler(tp, template),
-			"sections": ["indexCard", "documentation", "settings", "style"],
+			"sections": ["indexCard", "documentation", "settings"],
+			"fields": ["url", "version"],
+			"defaults": {
+				"tags": "⚙️/💄",
+			},
 		},
-		"snippets": {
+		"Snippets": {
 			"label": "💄 Revisión de CSS Snippets",
 			"folder": "A-Utility/A.5-Snippets",
 			"handler": async (tp, template) => await tp.user.snippetsHandler(tp, template),
-			"sections": ["indexCard"],
+			"sections": ["indexCard","documentation", "settings"],
+			"fields": ["url", "version"],
+			"defaults": {
+				"tags": "⚙️/💄",
+			},
 		},
 
 		// ✍️ WRITING
-		"writing": {
+		"Writing": {
 			"label": "✍️ Escrito",
 			"folder": "7-Crisol-de-Escritura/7.A-Manuscritos",
 			"handler": async (tp, template) => await tp.user.writingHandler(tp, template),
-			"sections": [],
+			"sections": ["indexCard"],
+			"fields": [],
+			"defaults": {
+				"tags": "✍️",
+				"publish": false,
+				"status": "📄",
+				"authors": "AtlcatlQ"
+			},
 		},
 
 		// 🗃️ ZETTELKASTEN
-		"capture": {
+		"Capture": {
 			"label": "📥 Nota de Captura",
 			"folder": "0-INBOX/0.1-Captura",
 			"handler": async (tp, template) => await tp.user.captureHandler(tp, template),
 			"sections": ["indexCard", "notas"],
+			"fields": ["title", "imported"],
+			"defaults": {
+				"tags": "📥",
+				"status": "🌑",
+				"priority": "🥈",
+				"type": "Otro",
+			},
+			"status": {
+				"Pendiente": "🌑",
+				"Leyendo": "🌘",
+				"Leído": "🌗",
+				"Integrando": "🌖",
+				"Procesado": "🌕",
+				"Previo": "🌔",
+				"Releer": "🌓",
+				"Abandonado": "🌒",
+				"Permanente": "🌙", 
+			},
 		},
-		"permanent": {
+		"Permanent": {
 			"label": "📓 Nota Permanente",
 			"folder": "0-INBOX",
 			"handler": async (tp, template) => await tp.user.noteHandler(tp, template),
 			"sections": [],
+			"fields": [],
+			"defaults": {
+				"tags": "📓",
+				"publish": false,
+				"status": "🌱"
+			},
+			"status": {
+				"Emergente": "🌱",
+				"Leyendo": "🪴",
+				"Evergreen": "🌲",
+			},
 		},
 		"default": {
 			// Este tipo es especial porque en realidad es un subtipo de nota de captura
@@ -94,6 +192,8 @@ module.exports = () => {
 			"folder": "",
 			"handler": async (tp, template) => await tp.user.captureHandler(tp, template),
 			"sections": [],
+			"fields": [],
+			"defaults": {},
 		},
 		*/
 	};
